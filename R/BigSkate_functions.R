c42c9a719fd3ff6e008fd74ef42f1027938395f4
Ian Taylor <ian.taylor@noaa.gov>
2019-04-28 07:00:26 -0700

    add simple wrapper for SS_output

---
+++ R/BigSkate_functions.R
@@ -0,0 +1,15 @@
+getbs <- function(n, plot = FALSE){
+  # function to figure out full path associated with a given model number,
+  # run SS_output and assign results to an object with a name like "bs9"
+  bsdir <- 'C:/SS/skates/models/'
+  mods <- dir(bsdir)
+  mod <- mods[grep(pattern = paste0("bigskate(0)*", n), mods)]
+  out <- SS_output(file.path(bsdir, mod))
+  assign(x=paste0("bs", n), value=out, pos=1)
+  if(plot){
+    SS_plots(out, res=200)
+  }
+}
+
+
+#SSplotPars(bs11, strings=rownames(bs11$estimated_non_dev_parameters)[bs11$estimated_non_dev_parameters$Pr_type!="No_prior"])
